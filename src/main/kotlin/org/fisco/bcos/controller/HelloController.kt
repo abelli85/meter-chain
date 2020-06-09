70c01dd08748658ef5e6dd1a9f1ecefbf976c2c9
abelli <abelli5@126.com>
2020-06-09 11:40:05 +0800

    模拟检定台自动上链

--- src/main/kotlin/org/fisco/bcos/controller/HelloController.kt
+++ src/main/kotlin/org/fisco/bcos/controller/HelloController.kt
@@ -1,5 +1,6 @@
 package org.fisco.bcos.controller
 
+import com.alibaba.fastjson.JSON
 import org.fisco.bcos.constants.GasConstants
 import org.fisco.bcos.model.Meter
 import org.fisco.bcos.model.MeterBatch
@@ -7,16 +8,19 @@ import org.fisco.bcos.temp.UserMeter
 import org.fisco.bcos.web3j.crypto.Credentials
 import org.fisco.bcos.web3j.protocol.Web3j
 import org.fisco.bcos.web3j.tx.gas.StaticGasProvider
+import org.joda.time.LocalDateTime
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.core.query.Criteria
 import org.springframework.data.mongodb.core.query.Query
+import org.springframework.data.mongodb.core.query.Update
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
+import java.util.*
 
 @Controller
 class HelloController {
@@ -80,6 +84,65 @@ class HelloController {
 
         // 展示链上合约
         return "meterResult"
+    }
+
+    @GetMapping("/autoChain")
+    fun autoChain(model: Model): String {
+        lgr.info("auto chain demo data.")
+
+        val batch = MeterBatch().apply {
+            batchId = "202006" + (1001..9999).random().toString()
+            meterList = listOf("0221520012" + (100001..999888).random().toString(),
+                    "0221520012" + (100001..999888).random().toString())
+            manufacturer = "宁波水表"
+            verifierName = "abel"
+            deliverDate = LocalDateTime(2020, 6, 1, 9, 20).toDate()
+            validDate = LocalDateTime(2026, 5, 31, 0, 0).toDate()
+        }
+
+        mongoTemplate!!.save(batch)
+        batch.meterList?.forEach {
+            mongoTemplate!!.save(Meter().apply {
+                meterId = it
+                batchId = batch.batchId
+            })
+        }
+        lgr.info("新到检定委托单: {}", JSON.toJSONString(batch, true))
+
+        // 委托单开始上链
+        val um = UserMeter.deploy(web3j, credentials,
+                StaticGasProvider(GasConstants.GAS_PRICE, GasConstants.GAS_LIMIT),
+                "宁波水表").send()
+        lgr.info("UserMeter contract address: {}", um.contractAddress)
+        batch.apply {
+            verifierAddress = um.verifier().send().toString()
+            contractAddress = um.contractAddress
+        }
+        mongoTemplate.updateFirst(
+                Query.query(Criteria.where("batchId").`is`(batch.batchId)),
+                Update.update("verifierAddress", batch.verifierAddress)
+                        .addToSet("contractAddress", batch.contractAddress),
+                MeterBatch::class.java)
+
+        um.verify(batch.meterList!![0].toByteArray(), LocalDateTime.now().toString(), "PASS").send()
+        Thread.sleep(100)
+        um.verify(batch.meterList!![1].toByteArray(), LocalDateTime.now().toString(), "PASS").send()
+        lgr.info("2 meters verified.")
+
+        // 完成委托单
+        um.finish().send()
+        batch.apply {
+            finishDate = Date()
+        }
+        mongoTemplate.updateFirst(
+                Query.query(Criteria.where("batchId").`is`(batch.batchId)),
+                Update.update("finishDate", batch.finishDate),
+                MeterBatch::class.java
+        )
+        lgr.info("完成委托单")
+
+        model.addAttribute("batch", batch)
+        return "autoResult"
     }
 
     companion object {
